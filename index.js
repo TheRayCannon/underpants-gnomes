4f17f5c2718141a7c11f34389db30c9ed660ac99
Shawn Cannon <scanno16@ford.com>
2022-04-07 16:22:29 -0600

    addEventListener

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function displayPlan(){
-  const step1 = "Collect underpants."
-  const step2 = "???"
-  const step3 = "Profit!"
+function displayPlan() {
+    const step1 = "Collect underpants."
+    const step2 = "???"
+    const step3 = "Profit!"
+    const firstStep = document.querySelector("#step-1")
+    const secondStep = document.querySelector("#step-2")
+    const thirdStep = document.querySelector("#step-3")
+    const button = document.querySelector("button")
+
+    button.addEventListener("click", event => {
+        firstStep.append(step1)
+        secondStep.append(step2)
+        thirdStep.append(step3)
+    })
 }
